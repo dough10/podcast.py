183cef51f37a25d4cd3669427db9804152226d8e
dough10 <doughten.jimi@gmail.com>
2024-12-08 17:03:29 -0600

    awdad

--- sh/podcast.sh
+++ sh/podcast.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-~/podcast.py/.venv/bin/python3 ~/podcast.py/podcast.py "$1" "$2"
+# Define the base command to run the Python script
+CMD="$HOME/podcast.py/.venv/bin/python3 ~/podcast.py/podcast.py"
+
+# If $1 is provided, append it to the command
+if [ -n "$1" ]; then
+    CMD="$CMD $1"
+fi
+
+# If $2 is provided, append it to the command
+if [ -n "$2" ]; then
+    CMD="$CMD $2"
+fi
+
+# Execute the command
+$CMD
