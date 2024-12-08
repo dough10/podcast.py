e441cb5fc53fe399b63bf9ffa85e41080613b35c
dough10 <doughten.jimi@gmail.com>
2024-12-08 17:07:48 -0600

    wad

--- sh/podcast.sh
+++ sh/podcast.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-# Define the base command to run the Python script
-CMD="$HOME/podcast.py/.venv/bin/python3 ~/podcast.py/podcast.py"
+CMD="$HOME/podcast.py/.venv/bin/python3 $HOME/podcast.py/podcast.py"
 
-# If $1 is provided, append it to the command
 if [ -n "$1" ]; then
     CMD="$CMD $1"
 fi
 
-# If $2 is provided, append it to the command
 if [ -n "$2" ]; then
     CMD="$CMD $2"
 fi
 
-# Execute the command
 $CMD
