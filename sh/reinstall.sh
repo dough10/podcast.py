84ee5628afa9d429a880fb69a322947b55966ffd
dough10 <doughten.jimi@gmail.com>
2024-12-08 18:56:44 -0600

    wad

--- sh/reinstall.sh
+++ sh/reinstall.sh
@@ -2,7 +2,7 @@
 
 [ -f ~/podcast.py/.env ] && cp -v ~/podcast.py/.env ~/.podcast.py-env
 bash ~/podcast.py/uninstall.sh
-cd ~/
+cd ~/ || exit
 curl -O https://raw.githubusercontent.com/dough10/podcast.py/refs/heads/main/install.sh
 bash install.sh
 rm install.sh
