9abcd8f35671950825ddfb90cd198581578bc5ea
dough10 <doughten.jimi@gmail.com>
2024-12-08 18:33:45 -0600

    OMG spam removing files

--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,6 @@
 set -e
 
 [ -f ~/.bashrc-backup ] && cp -v ~/.bashrc-backup ~/.bashrc && rm ~/.bashrc-backup
-rm -rfv ~/podcast.py
+rm -rf ~/podcast.py
 rm -v ~/podcast.log
 sudo rm -fv /usr/local/bin/podcast.py*
