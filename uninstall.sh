245a98886e039ac09eaa394a52f89ce77dde5952
dough10 <doughten.jimi@gmail.com>
2024-12-12 17:52:52 -0600

    wadwad

--- uninstall.sh
+++ uninstall.sh
@@ -4,6 +4,6 @@ set -e
 
 [ -f ~/.bashrc-backup ] && cp -v ~/.bashrc-backup ~/.bashrc && rm ~/.bashrc-backup
 rm -rf ~/podcast.py
-rm -v ~/podcast.log
+rm -vf ~/podcast.log
 sudo rm -fv /usr/local/bin/podcast.py*
 crontab -l | grep -v "podcast.py" | crontab -
